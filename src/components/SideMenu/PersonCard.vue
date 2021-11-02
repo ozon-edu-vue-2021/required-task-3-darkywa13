aae22c51b3c8c9b810fef7403a104b01290dda7e
Kirilenko A <kirilenkoartem13@ya.ru>
2021-11-02 21:49:41 +0300

    [feature/map] - added select person card and optimization json data

--- src/components/SideMenu/PersonCard.vue
+++ src/components/SideMenu/PersonCard.vue
@@ -1,60 +1,130 @@
 <template>
-    <div class="person">
-        <div class="person__photo">
-            <img
-                :src="person.picture"
-                alt="photo"
-            />
-        </div>
-        <div class="person__info">
-            <div class="person__info-name">
-                <b>{{ person.name }} ({{ person.age }})</b>
-            </div>
-
-            <div class="person__info-email">Почта: {{ person.email }}</div>
-            <div class="person__info-email">
-                Дата регистрации: {{ formatedDate }}
-            </div>
-            <div class="person__info-about">О себе: {{ person.about }}</div>
-        </div>
+  <div class="person">
+    <div class="person__photo">
+      <img :src="person.picture" alt="photo" />
+      <a href="#" class="action action_grey" type="button" target="_blink">
+        <span class="icon material-icons-outlined"> house </span>
+      </a>
     </div>
+    <div class="person__info">
+      <div class="person__info-name">
+        <b>{{ person.name }} ({{ person.age }})</b>
+      </div>
+
+      <div class="person__info-date">Дата: {{ formatedDate }}</div>
+      <div class="person__info-depart">Почта: {{ person.email }}</div>
+
+      <div class="person__info-actions">
+        <a
+          v-if="person.email"
+          :href="'mailto:' + person.email"
+          class="action"
+          type="button"
+          target="_blink"
+        >
+          <span class="icon material-icons-outlined"> email </span>
+        </a>
+        <a href="#" class="action" type="button" target="_blink">
+          <span class="action__label">K3</span>
+          <span class="icon material-icons-outlined"> link </span>
+        </a>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import { format } from "date-fns";
+
 export default {
-    props: {
-        person: {
-            type: Object,
-            default: null,
-        },
+  props: {
+    person: {
+      type: Object,
+      required: true,
     },
-    computed: {
-        formatedDate() {
-            return this.person.registered;
-        },
+  },
+  computed: {
+    formatedDate() {
+      return format(new Date(this.person.registered), "dd.MM.yyyy hh:mm");
     },
+  },
 };
 </script>
 
 <style scoped>
 .person {
-    display: grid;
-    grid-template-columns: 50px 1fr;
-    grid-gap: 8px;
+  display: grid;
+  grid-template-columns: 200px minmax(0, 1fr);
+  grid-gap: 15px;
+}
+
+.person__photo {
+  position: relative;
+}
+
+.person__photo .action {
+  position: absolute;
+  left: -10px;
+  bottom: 10px;
 }
 
 .person__photo img {
-    height: 50px;
-    width: 50px;
-    border-radius: 50%;
+  height: 200px;
+  width: 200px;
+  display: block;
+  border-radius: 5px;
 }
 
 .person__info {
-    display: grid;
-    grid-gap: 8px;
+  display: flex;
+  flex-direction: column;
 }
 
 .person__info-name {
-    margin-bottom: 10px;
+  color: #2859f1;
+  font-size: 18px;
+  margin-bottom: 10px;
+}
+
+.person__info-date {
+  font-weight: bold;
+  margin-bottom: 10px;
+  font-size: 16px;
+}
+
+.person__info-depart {
+  color: #a3a3a3;
+  font-size: 16px;
+}
+
+.person__info-actions {
+  display: flex;
+  contain: content;
+  justify-content: space-between;
+  flex: 1;
+  align-items: self-end;
+  padding-bottom: 10px;
+}
+
+.action {
+  display: inline-flex;
+  align-items: center;
+  background-color: #e3eaff;
+  text-decoration: none;
+  border-radius: 5px;
+  padding: 5px;
+  color: #2859f1;
+}
+
+.action__label {
+  font-weight: bold;
+  margin-right: 10px;
+}
+
+.action.action_grey {
+  background-color: rgb(68 68 68 / 50%);
+  -webkit-backdrop-filter: blur(140px);
+  backdrop-filter: blur(20px);
+  color: #fff;
 }
 </style>
---
+++ src/components/SideMenu/SidePerson.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="profile">
+    <div v-if="!person" class="profile__empty">Место пустое</div>
+
+    <PersonCard v-else :person="person" />
+  </div>
+</template>
+
+<script>
+import PersonCard from "./PersonCard.vue";
+
+export default {
+  props: {
+    person: {
+      type: Object,
+      default: null,
+    },
+  },
+  components: { PersonCard },
+};
+</script>
+
+<style scoped>
+.profile__empty {
+  text-align: center;
+  font-weight: bold;
+}
+</style>
